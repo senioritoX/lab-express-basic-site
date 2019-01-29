d98757ab00ee838595d0d5096ed9bc6b0519008f
Mr.X <seniorito.x@icloud.com>
2019-01-29 23:30:28 +0100

    Added Style pending on adding content😪

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,8 +6,15 @@ app.use(express.static('public'))
 
 
 app.get('/', (request, response, next) => {
-  console.log(request);
-  response.sendFile(__dirname + '/views/home.html');
-});
+  response.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (request, response, next)=> {
+  response.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/views/gallery.html')
+})
 
 app.listen(3000)
